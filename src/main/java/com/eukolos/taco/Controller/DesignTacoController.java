e52371903ccfa1d5d127d051a5702be845dde681
eukolos <eminaksoy35@gmail.com>
2022-07-26 13:03:48 +0300

    Step 2.2

--- src/main/java/com/eukolos/taco/Controller/DesignTacoController.java
+++ src/main/java/com/eukolos/taco/Controller/DesignTacoController.java
@@ -1,37 +1,44 @@
 package com.eukolos.taco.Controller;
 
 
-import com.eukolos.taco.Model.TacoModel;
-import com.eukolos.taco.Model.TacoModel.Type;
+import com.eukolos.taco.Model.Taco;
+import com.eukolos.taco.Model.Ingredient;
+import com.eukolos.taco.Model.Ingredient.Type;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
     @GetMapping
     public String showDesignForm(Model model) {
-        List<TacoModel> ingredients = Arrays.asList(
-                new TacoModel("FLTO", "Flour Tortilla", Type.WRAP),
-                new TacoModel("COTO", "Corn Tortilla", Type.WRAP),
-                new TacoModel("GRBF", "Ground Beef", Type.PROTEIN),
-                new TacoModel("CARN", "Carnitas", Type.PROTEIN),
-                new TacoModel("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new TacoModel("LETC", "Lettuce", Type.VEGGIES),
-                new TacoModel("CHED", "Cheddar", Type.CHEESE),
-                new TacoModel("JACK", "Monterrey Jack", Type.CHEESE),
-                new TacoModel("SLSA", "Salsa", Type.SAUCE),
-                new TacoModel("SRCR", "Sour Cream", Type.SAUCE)
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
-        Type[] types = TacoModel.Type.values();
+        Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
@@ -50,9 +57,9 @@ public class DesignTacoController {
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
 
-        return "redirect:/orders/current";
+        return "redirect:/";
     }
-    private List<TacoModel> filterByType(List<TacoModel> ingredients, Type type) {
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 
         return ingredients.stream()
                 .filter(x -> x.getType().equals(type))
--- src/test/java/com/eukolos/taco/TacoControllerTests.java
+++ src/test/java/com/eukolos/taco/TacoControllerTests.java
@@ -1,15 +1,5 @@
 package com.eukolos.taco;
 
-import static org.hamcrest.Matchers.containsString;
-import static
-        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static
-        org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import com.eukolos.taco.Controller.TacoController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @RunWith(SpringRunner.class)
 @WebMvcTest(TacoController.class)
 public class TacoControllerTests {
