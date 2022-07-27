eed033210cf4af146c72cf9b133deb62c476872a
eukolos <eminaksoy35@gmail.com>
2022-07-27 17:33:57 +0300

    Step 2.4 (WenConfig)

--- src/main/java/com/eukolos/taco/Controller/OrderController.java
+++ src/main/java/com/eukolos/taco/Controller/OrderController.java
@@ -5,9 +5,12 @@ import com.eukolos.taco.Model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -18,9 +21,14 @@ public class OrderController {
         model.addAttribute("order", new Order());
         return "orderForm";
     }
+
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
+
 }
